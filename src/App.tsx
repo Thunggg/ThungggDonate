587703d17c4f8ce63bf80e56b32035f17854bd6d
Thunggg <thuannguyen20041028@gmail.com>
2025-05-22 02:26:57 +0700

    goi function fund để donate

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ExternalLink } from 'lucide-react';
 import { BrowserProvider, Contract, formatUnits } from "ethers";
 import type { Eip1193Provider } from "ethers";
 import { formatEther } from 'ethers';
+import { parseEther } from 'ethers';
 
 // 1. Get projectId
 const projectId = import.meta.env.VITE_WALLET_CONNECT_ID;
@@ -39,9 +40,9 @@ function App() {
   const { open, close } = useAppKit();
   const { address, isConnected, caipAddress, status, embeddedWalletInfo } = useAppKitAccount();
   const { walletProvider } = useAppKitProvider("eip155");
-  const [crownfundingBal, setCrownfundingBal] = useState<string | null>("");
-  const [funderLength, setFunderLength] = useState<number | null>(0);
-
+  const [crownfundingBal, setCrownfundingBal] = useState<string | null>(null);
+  const [funderLength, setFunderLength] = useState<number | null>(null);
+  const [amountFund, setAmoundFund] = useState<number | null>(null);
 
   const fetchContractBalance = async () => {
     if (walletProvider) {
@@ -50,19 +51,39 @@ function App() {
       const ethersProvider = new BrowserProvider(provider);
       const contractBalance = await ethersProvider.getBalance(contracAddress);
       setCrownfundingBal(formatEther(contractBalance));
-      console.log(crownfundingBal);
 
       // lấy số người đã đóng góp
       const contract = new Contract(contracAddress, contractABI, ethersProvider);
-      const funderLength = await contract.getFunderLength();
+      const funderLength = await contract.getFundersLength();
       setFunderLength(funderLength);
-      console.log(funderLength);
     }
+  }
+
+
+  const handleFundToCrownFunding = async () => {
+    if (amountFund == null || amountFund <= 0) {
+      alert("Giá trị không hơp lệ!");
+      return;
+    }
+    if (walletProvider) {
+      // lấy balance của contract
+      const provider = walletProvider as Eip1193Provider;
+      const ethersProvider = new BrowserProvider(provider);
+      const signer = await ethersProvider.getSigner();
+      const contract = new Contract(contracAddress, contractABI, signer);
+      const tx = await contract.fund({ value: parseEther(amountFund.toString()) });
+      await tx.wait();
+      fetchContractBalance();
+    }
+  }
+
+  const onInputAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmoundFund(Number(e.target.value));
   }
 
   useEffect(() => {
     fetchContractBalance();
-  });
+  }, []);
 
   return (
     <>
@@ -95,22 +116,35 @@ function App() {
       <main className='pt-4 container mx-auto px-2'>
 
         <div className='flex justify-start items-center'>
-          <div className='space-y-2 w-[30%]'>
-            <div className='p-2 shadow-lg  rounded-lg'>
-              <h2 className='text-lg'>Total amount funding</h2>
-              <span className='text-2xl font-bold'>{parseInt(crownfundingBal ?? '0') === 0.0 ? 0 : crownfundingBal} </span> <span>ETH</span>
-            </div>
 
-            <div className='p-2 shadow-lg  rounded-lg'>
+          <div className='space-y-2 w-[30%]'>
+            <div className='px-2 py-4 shadow-lg  rounded-lg'>
+              <h2 className='text-lg'>Total amount funding</h2>
+              <span className='text-2xl font-bold'>{crownfundingBal} </span> <span>ETH</span>
+            </div>
+            <div className='px-2 py-4 shadow-lg  rounded-lg'>
               <h2 className='text-lg'>Funder</h2>
               <span className='text-2xl font-bold'>{funderLength}</span>
             </div>
           </div>
+
+
+          <div className='w-[70%] px-2 py-4 ml-2  shadow-lg  rounded-lg'>
+            <h2 className='text-lg font-semibold'>Donate your ethers</h2>
+            <input
+              placeholder='0.0'
+              className='border p-2 rounded-lg '
+              type='number'
+              onChange={onInputAmountChange}
+            />
+            <button
+              className='bg-slate-900 text-white py-2 px-3 rounded-lg hover:bg-slate-800'
+              onClick={() => handleFundToCrownFunding()}
+            >Donate</button>
+          </div>
         </div>
 
-        <div className='flex justify-start items-center'>
 
-        </div>
       </main >
     </>
   );
